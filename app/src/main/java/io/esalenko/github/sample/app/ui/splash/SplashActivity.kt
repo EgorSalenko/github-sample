7bf2ed4707a952fec530ed9ce1fb76539c5795b5
Egor Salenko <wrongua@gmail.com>
2020-02-05 20:57:47 +0200

    Add token validity todo

--- app/src/main/java/io/esalenko/github/sample/app/ui/splash/SplashActivity.kt
+++ app/src/main/java/io/esalenko/github/sample/app/ui/splash/SplashActivity.kt
@@ -22,6 +22,8 @@ class SplashActivity : BaseActivity(R.layout.activity_splash) {
 
     private fun subscribeUi() {
         splashViewModel.tokenLiveData.observe(this, Observer { event ->
+            // TODO :: check token validity
+            // https://developer.github.com/v3/apps/oauth_applications/
             when (event.getContentIfNotHandled()) {
                 is SplashViewModel.TokenResult.Valid -> openSearchScreen()
                 is SplashViewModel.TokenResult.Empty -> openLoginScreen()
